f2299bd40da64f45789d15157147f88df86388fc
wallabyway <bealester@gmail.com>
2023-03-22 12:13:15 +0200

    initial commit

---
+++ wwwroot/main.js
@@ -0,0 +1,36 @@
+import { initViewer, loadModel, loadTiles } from './viewer.js';
+import { initTree } from './sidebar.js';
+
+const login = document.getElementById('login');
+try {
+    const resp = await fetch('/api/auth/profile');
+    if (resp.ok) {
+        const user = await resp.json();
+        login.innerText = `Logout (${user.name})`;
+        login.onclick = () => {
+            const iframe = document.createElement('iframe');
+            iframe.style.visibility = 'hidden';
+            iframe.src = 'https://accounts.autodesk.com/Authentication/LogOut';
+            document.body.appendChild(iframe);
+            iframe.onload = () => {
+                window.location.replace('/api/auth/logout');
+                document.body.removeChild(iframe);
+            };
+        }
+        const viewer = await initViewer(document.getElementById('preview'));
+        initTree('#tree', (id, ext) => {
+			const urn = window.btoa(id).replace(/=/g, '')
+			if (ext == "rcs") 
+				loadTiles(viewer, urn)
+			else
+				loadModel(viewer, urn)
+		});
+    } else {
+        login.innerText = 'Login';
+        login.onclick = () => window.location.replace('/api/auth/login');
+    }
+    login.style.visibility = 'visible';
+} catch (err) {
+    alert('Could not initialize the application. See console for more details.');
+    console.error(err);
+}
